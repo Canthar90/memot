4bd32fe95ee885b616459c87cd5cb042d1d7f6a3
Canthar90 <d.cieslak90@gmail.com>
2022-04-03 12:27:35 +0200

    Testing Working with simple csv containing dates of events

--- dates_test.py
+++ dates_test.py
@@ -42,28 +42,54 @@ future = future.strftime("%d/%m/%Y")
 
 date = date.strftime("%d/%m/%Y")
 
-message = ''
+message = 'Upcoming trash : \n '
 
 matching_plastik_df =plastiki_df[(plastiki_df.plastiki >= pd.to_datetime(date, dayfirst=True)) &
                                  (plastiki_df.plastiki <= pd.to_datetime(future, dayfirst=True))]
 
 if not matching_plastik_df.empty:
-    message += f"Plastiki {matching_plastik_df.plastiki.dt.strftime('%Y-%m-%d').values[0]} it's: " \
-               f"{str(matching_plastik_df.plastiki.dt.day_name().values[0])} \n"
+    message += f"Plastiki {matching_plastik_df.plastiki.dt.strftime('%d-%m-%Y').values[0]} " \
+               f" it's: {(matching_plastik_df.plastiki.dt.day_name().values[0])} \n"
 
 # print(f'pasujące {matching_plastik_df}')
 # matching_plastik_df = matching_plastik_df[(plastiki_df.plastiki <= pd.to_datetime(future, dayfirst=True))]
 
 matching_mieszane_df = mieszane_df[(mieszane_df.mieszane >= pd.to_datetime(date, dayfirst=True)) &
                                    (mieszane_df.mieszane <= pd.to_datetime(future, dayfirst=True))]
+if not matching_mieszane_df.empty:
+    message += f"Mieszane {matching_mieszane_df.mieszane.dt.strftime('%d-%m-%Y').values[0]} " \
+               f"it's: {matching_mieszane_df.mieszane.dt.day_name().values[0]} \n"
 
 
+
+matching_bio_df = bio_df[(bio_df.bio >= pd.to_datetime(date, dayfirst=True)) &
+                         (bio_df.bio <= pd.to_datetime(future, dayfirst=True))]
+
+if not matching_bio_df.empty:
+    message += f"Bio {matching_bio_df.bio.dt.strftime('%d-%m-%Y').values[0]} " \
+               f"it's: {matching_bio_df.bio.dt.day_name().values[0]} \n"
+
+
+matching_szklo_df = szklo_df[(szklo_df.szklo >= pd.to_datetime(date, dayfirst=True)) &
+                             (szklo_df.szklo <= pd.to_datetime(future, dayfirst=True))]
+
+if not matching_szklo_df.empty:
+    message += f"Szkło {matching_szklo_df.szklo.dt.strftime('%d-%m-%Y').values[0]} " \
+               f"it's: {matching_szklo_df.szklo.dt.day_name().values[0]} \n"
 
 
 matching_gabaryty_df = gabaryt_df[(gabaryt_df.gabaryt >= pd.to_datetime(date, dayfirst=True)) &
                                   (gabaryt_df.gabaryt <= pd.to_datetime(future, dayfirst=True))]
-if matching_gabaryty_df.empty:
-    matching_gabaryty_df = 'Nic tu nie ma pusto'
+if not matching_gabaryty_df.empty:
+    message += f"Gabaryty {matching_gabaryty_df.gabatyt.dt.strftime('%d-%m-%Y').values[0]}" \
+               f"it's: {matching_gabaryty_df.gabaryty.dt.day_name().values[0]} \n"
+
+matching_popiol_df = popiol_df[(popiol_df.popiol >= pd.to_datetime(date, dayfirst=True)) &
+                              (popiol_df.popiol <= pd.to_datetime(future, dayfirst=True))]
+
+if not matching_popiol_df.empty:
+    message += f"Popiół {matching_popiol_df.popiol.dt.strftime('%d-%m-%Y').values[0]} " \
+               f"it's: {matching_popiol_df.popiol.dt.day_name().values[0]} \n"
 
 
 
