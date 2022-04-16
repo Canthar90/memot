76164fa30fed8ff72301d86c973df7e5b6139d00
Canthar90 <d.cieslak90@gmail.com>
2022-04-16 18:12:10 +0200

    debugging

---
+++ apis.py
@@ -0,0 +1,68 @@
+import requests
+import datetime
+import json
+
+
+
+class Weather_forecasting():
+    def __init__(self):
+        with open("places.json") as file:
+            self.places = json.load(file)
+
+    def weather_check(self):
+        WEATHER_API = "https://api.openweathermap.org/data/2.5/onecall?"
+
+
+        api_key = "key"
+        for city in self.places:
+
+            parameters = {
+                "lat": self.places[city][0],
+                "lon": self.places[city][1],
+                "exclude": "current,minutely,daily",
+                "appid": api_key,
+                "units": 'metric',
+                "lang": "pl"
+            }
+
+
+
+            response = requests.get(WEATHER_API, params=parameters)
+            response.raise_for_status()
+            weather_data = response.json()
+
+
+            gonna_rain = False
+            weather_hourly = weather_data["hourly"]
+            weather_future = weather_hourly[:12]
+            time = datetime.datetime.now()
+
+
+
+            for hour_data in weather_future:
+                weather_id = hour_data["weather"][0]["id"]
+                if int(weather_id) <700:
+                    gonna_rain = True
+
+
+            if gonna_rain:
+                print(f"Prognoza pogody dla miasta {city}\n"
+                      f"Obecna temperatura to: {weather_hourly[0]['temp']}, odczuwalna: {weather_hourly[0]['feels_like']}, "
+                      f"ciśnienie: {weather_hourly[0]['pressure']}, widoczność: {weather_hourly[0]['visibility']}m, "
+                      f"prędkość watru: {weather_hourly[0]['wind_speed']}km/h, UWAGA PADA DYSZCZ!!!!!! \n"
+                      f"Przyszła temperatura to: {weather_hourly[6]['temp']}, odczuwalna: {weather_hourly[6]['feels_like']}, "
+                      f"ciśnienie: {weather_hourly[6]['pressure']}, widoczność: {weather_hourly[6]['visibility']}m, "
+                      f"prędkość watru: {weather_hourly[6]['wind_speed']}km/h, {weather_hourly[6]['weather'][0]['description']}")
+
+
+            else:
+                print(f"Prognoza pogody dla miasta {city}\n"
+                      f"Obecna temperatura to: {weather_hourly[0]['temp']}, odczuwalna: {weather_hourly[0]['feels_like']}, "
+                        f"ciśnienie: {weather_hourly[0]['pressure']}, widoczność: {weather_hourly[0]['visibility']}m, "
+                        f"prędkość watru: {weather_hourly[0]['wind_speed']}km/h, Nie pada,"
+                      f" {weather_hourly[0]['weather'][0]['description']} \n"
+                      f"Przyszła temperatura to: {weather_hourly[6]['temp']}, odczuwalna: {weather_hourly[6]['feels_like']}, "
+                      f"ciśnienie: {weather_hourly[6]['pressure']}, widoczność: {weather_hourly[6]['visibility']}m, "
+                      f"prędkość watru: {weather_hourly[6]['wind_speed']}km/h, Nie pada,"
+                      f" {weather_hourly[6]['weather'][0]['description']}")
+
