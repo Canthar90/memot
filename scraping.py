25de819f33e817f0e483fa282cd8132cb45a5bb5
Canthar90 <d.cieslak90@gmail.com>
2022-08-28 11:37:10 +0200

    Added tests for invalid arguments and raising errors in Scraping class

--- scraping.py
+++ scraping.py
@@ -44,95 +44,103 @@ class Allegro_scrapping():
             self.planned = {}
 
 
-    def search(self, search_word, search_number=1, by_pricve=False):
+    def search(self, search_word: str, search_number: int=1, by_pricve: bool=False) -> str:
         '''this function will search the search_word and retutrn message containing selected search_number of
             search reasults '''
 
-        chrome_driver = "./chromedriver/chromedriver.exe"
-        s = Service(chrome_driver)
-        options = ChromeOptions()
-        driver = webdriver.Chrome(service=s, options=options)
-        action = ActionChains(driver=driver)
-        wait = WebDriverWait(driver, 10)
-        
-
-        driver.get("https://allegro.pl")
-
-        cookies_agreement = wait.until(EC.element_to_be_clickable((By.XPATH,
-         """//*[@id="opbox-gdpr-consents-modal"]/div/div[2]/div/div[2]/button[1]""")))
-        
-        action.click(on_element=cookies_agreement)
-
-        
-        
-        
-        search_pole = wait.until(EC.visibility_of_element_located((
-            By.XPATH, "/html/body/div[3]/div[2]/div/div/div/div/div/div[3]/header/div/div/div/div/form/input"
-        )))
-
-        
-
-        action.click(on_element=search_pole)
-        action.send_keys(search_word)
-        action.key_down(Keys.ENTER)
-        action.key_up(Keys.ENTER)
-        action.perform()
-
-        if by_pricve:
+        if type(search_word) != str:
+            raise ValueError("Invalid data type in search_word expected str")
+        elif type(search_number) != int:
+            raise ValueError("Invalid data type in search_number expected int")
+        elif type(by_pricve) != bool:
+            raise ValueError("Invalid data type in by_price expected bool")
+        else:
             
-            sorting_selector = wait.until(EC.visibility_of_element_located((
-                By.XPATH,
-                """//*[@id="allegro.listing.sort"]""")))
+            chrome_driver = "./chromedriver/chromedriver.exe"
+            s = Service(chrome_driver)
+            options = ChromeOptions()
+            driver = webdriver.Chrome(service=s, options=options)
+            action = ActionChains(driver=driver)
+            wait = WebDriverWait(driver, 10)
             
-            action.click(on_element=sorting_selector)
-            action.key_down(Keys.ARROW_DOWN)
-            action.key_up(Keys.ARROW_DOWN)
+
+            driver.get("https://allegro.pl")
+
+            cookies_agreement = wait.until(EC.element_to_be_clickable((By.XPATH,
+            """//*[@id="opbox-gdpr-consents-modal"]/div/div[2]/div/div[2]/button[1]""")))
+            
+            action.click(on_element=cookies_agreement)
+
+            
+            
+            
+            search_pole = wait.until(EC.visibility_of_element_located((
+                By.XPATH, "/html/body/div[3]/div[2]/div/div/div/div/div/div[3]/header/div/div/div/div/form/input"
+            )))
+
+            
+
+            action.click(on_element=search_pole)
+            action.send_keys(search_word)
             action.key_down(Keys.ENTER)
             action.key_up(Keys.ENTER)
             action.perform()
-            time.sleep(2)
-            
-            
-            
 
-        try:
-            invalid = driver.find_element(By.XPATH,
-                """/html/body/div[2]/div[4]/div/div/div/div/div/div[2]/section/h1""")
-            
-            return 'Sory nie mogę znaleźć wyników dla tego zapytania'
-            
-        except:
+            if by_pricve:
+                
+                sorting_selector = wait.until(EC.visibility_of_element_located((
+                    By.XPATH,
+                    """//*[@id="allegro.listing.sort"]""")))
+                
+                action.click(on_element=sorting_selector)
+                action.key_down(Keys.ARROW_DOWN)
+                action.key_up(Keys.ARROW_DOWN)
+                action.key_down(Keys.ENTER)
+                action.key_up(Keys.ENTER)
+                action.perform()
+                time.sleep(2)
+                
+                
+                
 
-            wait.until(EC.visibility_of_element_located((By.XPATH,
-                """//*[@id="search-results"]/div[6]/div/div/div[1]/div/div/section/article/div/div/div[2]/div[1]/h2/a""")))
-
-            main_titles = driver.find_elements(
-                By.XPATH,
-                """//*[@id="search-results"]/div[6]/div/div/div[1]/div/div/section/article/div/div/div[2]/div[1]/h2/a""")
-            
-
-            main_prices = driver.find_elements(
-                By.XPATH, 
-                """//*[@id="search-results"]/div[6]/div/div/div[1]/div/div/section/article/div/div/div[2]/div[2]/div/div/span""")
-            
-            
-
-            if len(main_titles) == 0:
+            try:
+                invalid = driver.find_element(By.XPATH,
+                    """/html/body/div[2]/div[4]/div/div/div/div/div/div[2]/section/h1""")
+                
                 return 'Sory nie mogę znaleźć wyników dla tego zapytania'
-            else:
-                message = ''
-                count = 0
-                for title in main_titles:
-                    message += f"""Tytuł to: {main_titles[count].text}\nCena to {main_prices[count].text}\n"""
-                    message += f"""link: {main_titles[count].get_attribute('href')} \n"""
-                    count += 1
-                    if count == int(search_number):
-                        driver.quit()
-                        return message
+                
+            except:
 
-                message += "Nie było wystarczająco dużo wyników wyszukiwania"
-                driver.quit()
-                return message
+                wait.until(EC.visibility_of_element_located((By.XPATH,
+                    """//*[@id="search-results"]/div[6]/div/div/div[1]/div/div/section/article/div/div/div[2]/div[1]/h2/a""")))
+
+                main_titles = driver.find_elements(
+                    By.XPATH,
+                    """//*[@id="search-results"]/div[6]/div/div/div[1]/div/div/section/article/div/div/div[2]/div[1]/h2/a""")
+                
+
+                main_prices = driver.find_elements(
+                    By.XPATH, 
+                    """//*[@id="search-results"]/div[6]/div/div/div[1]/div/div/section/article/div/div/div[2]/div[2]/div/div/span""")
+                
+                
+
+                if len(main_titles) == 0:
+                    return 'Sory nie mogę znaleźć wyników dla tego zapytania'
+                else:
+                    message = ''
+                    count = 0
+                    for title in main_titles:
+                        message += f"""Tytuł to: {main_titles[count].text}\nCena to {main_prices[count].text}\n"""
+                        message += f"""link: {main_titles[count].get_attribute('href')} \n"""
+                        count += 1
+                        if count == int(search_number):
+                            driver.quit()
+                            return message
+
+                    message += "Nie było wystarczająco dużo wyników wyszukiwania"
+                    driver.quit()
+                    return message
 
 
 
--- tests/test_scraping.py
+++ tests/test_scraping.py
@@ -27,7 +27,7 @@ def test_pancerni_scrapping_fails():
     
 # -----------------Test Allegro scraping-------------------------
 @pytest.mark.parametrize("keyword,nr_of_results,flag", [
-    ("kopytko", 1, ""), ("kopytko", 10, ""), ("kopytko", 10, True)
+    ("kopytko", 1, False), ("kopytko", 10, False), ("kopytko", 10, True)
 ])    
 def test_allegro_scrapping_passes_with_valid_arguments(keyword, nr_of_results, flag):
     """Test if Allegro scraping works correctly with two valid arguments passed"""
@@ -37,10 +37,23 @@ def test_allegro_scrapping_passes_with_valid_arguments(keyword, nr_of_results, f
     
     
 @pytest.mark.parametrize("keyword,nr_of_results,flag", [
-    ("adweqeedasfaw", 1, False), ("kopytko", 0, False)
+    ("adweqeedasfaw", 1, False), ("kopytko", 0, False),
 ])
 def test_allegro_scrapping_fails_nonsence_arguments(keyword, nr_of_results, flag):
+    """Test if Allegro scraping fails with invalid arguments"""
     allegro_scraping = Allegro_scrapping()
     resp = allegro_scraping.search(keyword, nr_of_results, flag)
     assert "Nie było wystarczająco dużo" in resp or "Sory nie mogę znaleźć" in resp
+    
+    
+@pytest.mark.parametrize("keyword,nr_of_results,flag", [
+    ("kopytko", "string", False), ("kopytko", 10, "string"), ("kopytko", False, False),
+    ("kopytko", 1, 123), (12313213214214123, 1, False),
+])
+def test_allegro_scrapping_fails_invalid_datatypes(keyword, nr_of_results, flag):
+    """Tet if Allegro scraping rises exception when bad data types passed"""
+    allegro_scraping = Allegro_scrapping()
+    with pytest.raises(ValueError) as err:
+        allegro_scraping.search(keyword, nr_of_results, flag)
+    assert "Invalid data type" in str(err.value)
     
